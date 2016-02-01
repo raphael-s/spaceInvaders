01980212cebc8a528e9579112427ee8084a77f90
raphael-s <r.schwob@4teamwork.ch>
2016-02-01 15:02:22 +0100

    Level systme no works. Game is now more or less balanced and playable.

--- py_files/main.py
+++ py_files/main.py
@@ -1,24 +1,29 @@
-from Tkinter import *
+from Tkinter import Canvas, IntVar, Frame, Tk
 from random import randint
 import tkFont
+import os
+from PIL import ImageTk, Image
 from alien import Alien
+
 
 WIDTH = 500
 HEIGHT = 750
 DELAY = 50
+ROOT_DIR = os.path.abspath(os.path.join(__file__, "..", ".."))
+
 
 class Board(Canvas):
     def __init__(self):
-        Canvas.__init__(self, width=WIDTH, height=HEIGHT, 
-            background="white", highlightthickness=0)
+        Canvas.__init__(self, width=WIDTH, height=HEIGHT, background="white", highlightthickness=0)
         self.initGame()
         self.pack()
 
     def initGame(self):
-        self.score = IntVar(self, 0)
-        self.level = IntVar(self, 1)
+        self.score = 0
+        self.level = 1
         self.health = 3
-        self.alienSpawn = 0
+        self.alienSpawnCount = 8
+        self.spawnDelay = 30
         self.shootCooldown = 0
         self.bind_all("<a>", self.moveLeft)
         self.bind_all("<d>", self.moveRight)
@@ -29,16 +34,21 @@ class Board(Canvas):
     def initObj(self):
         self.menu = []
         menuFont = tkFont.Font(size="20")
-        #self.spaceShipImage = imageTK.PhotoImage(file="../gfx/green.png")
+        #healthimg = ImageTk.PhotoImage(file=ROOT_DIR + "/gfx/blue.png")
+        #self.healthimg = healthimg
         self.menu.append(self.create_rectangle(0, 0, WIDTH, 30, width=0, fill="grey", tag="menuBar"))
         self.menu.append(self.create_text(5, 5, text="Score: ", anchor="nw", font=menuFont, tag="scoreLabel"))
-        self.menu.append(self.create_text(self.coords(self.find_withtag("scoreLabel"))[0]+65, 5, text=str(self.score.get()), anchor="nw", font=menuFont, tag="score"))
+        self.menu.append(self.create_text(self.coords(self.find_withtag("scoreLabel"))[0] + 65, 5, text=str(self.score), anchor="nw", font=menuFont, tag="score"))
         self.menu.append(self.create_text(205, 5, text="Level: ", anchor="nw", font=menuFont, tag="levelLabel"))
-        self.menu.append(self.create_text(self.coords(self.find_withtag("levelLabel"))[0]+63, 5, text=str(self.level.get()), anchor="nw", font=menuFont, tag="level"))
+        self.menu.append(self.create_text(self.coords(self.find_withtag("levelLabel"))[0] + 63, 5, text=str(self.level), anchor="nw", font=menuFont, tag="level"))
         for i in range(self.health):
             count = i + 1
-            self.menu.append(self.create_rectangle(WIDTH - (25 * count), 5, WIDTH - (25 * count) + 20, 25, fill="red", tag="health"+str(count)))
+            #self.menu.append(self.create_image(WIDTH - (25 * count), 5, image=healthimg, tag="health" + str(count)))
+            self.menu.append(self.create_rectangle(WIDTH - (25 * count), 5, WIDTH - (25 * count) + 20, 25, fill="red", tag="health" + str(count)))
+        #img = ImageTk.PhotoImage(file=ROOT_DIR + "/gfx/blue2.png")
         self.spaceship = self.create_rectangle(0, HEIGHT-50, 50, HEIGHT, tag="spaceship", fill="green")
+        #self.spaceship = self.create_image(WIDTH / 2 + 25, HEIGHT - 50, image=img, tag="spaceship")
+        #self.spaceimg = img
         self.alienList = []
         self.shotList = []
 
@@ -59,7 +69,7 @@ class Board(Canvas):
         for shot in self.shotList:
             shotx = range(int(self.coords(shot[0])[0]), int(self.coords(shot[0])[0]) + shot[1].sizex)
             shoty = range(int(self.coords(shot[0])[1]), int(self.coords(shot[0])[1]) + shot[1].sizey)
-            if self.coords(shot[0])[1] <= 0 or self.coords(shot[0])[1] >= HEIGHT:
+            if self.coords(shot[0])[1] <= 30 or self.coords(shot[0])[1] >= HEIGHT:
                 remShotList.append(shot)
 
             if not shot[0] in self.find_withtag("alienShot"):
@@ -81,7 +91,7 @@ class Board(Canvas):
                         for y in shoty:
                             if y in shipy:
                                 remShotList.append(shot)
-                                remHealthList.append(self.find_withtag("health"+str(self.health)))
+                                remHealthList.append(self.find_withtag("health" + str(self.health)))
 
         if len(remAlienList) > 0:
             remAlienList = set(remAlienList)
@@ -104,8 +114,8 @@ class Board(Canvas):
         for item in elements:
             del self.alienList[self.alienList.index(item)]
             self.delete(item[0])
-            self.score.set(self.score.get()+1)
-            self.itemconfigure(self.find_withtag("score"), text=self.score.get())
+            self.score += 1
+            self.itemconfigure(self.find_withtag("score"), text=self.score)
 
     def remHealth(self, elements):
         for item in elements:
@@ -122,13 +132,13 @@ class Board(Canvas):
     def doShoot(self):
         if self.shootCooldown > 0:
             self.shootCooldown -= 1
-        if len(self.alienList) >= 10:
-            rand = randint(1,100)
-            if rand == 1:
-                randAlien = self.alienList[randint(0, len(self.alienList)-1)]
-                shotPosx = self.coords(randAlien[0])[0] + (randAlien[1].sizex/2) -1
-                shotPosy = self.coords(randAlien[0])[1] + randAlien[1].sizey
-                self.shotList.append((self.create_rectangle(shotPosx, shotPosy, shotPosx+2, shotPosy+4, width=1, tag="alienShot"), Shot(shotPosx, 2, 4, 0, 3)))
+
+        rand = randint(1, 100 / self.level * 1.5)
+        if rand == 1:
+            randAlien = self.alienList[randint(0, len(self.alienList) - 1)]
+            shotPosx = self.coords(randAlien[0])[0] + (randAlien[1].sizex / 2) - 1
+            shotPosy = self.coords(randAlien[0])[1] + randAlien[1].sizey
+            self.shotList.append((self.create_rectangle(shotPosx, shotPosy, shotPosx + 2, shotPosy + 4, width=1, tag="alienShot"), Shot(shotPosx, 2, 4, 0, 3)))
 
     def moveRight(self, e):
         if self.coords(self.spaceship)[0] + 55 <= WIDTH:
@@ -138,43 +148,53 @@ class Board(Canvas):
         if self.coords(self.spaceship)[0] - 5 >= 0:
             self.move(self.spaceship, -5, 0)
 
-    def onChangeScore(self, arg1, arg2, arg3):
-        self.itemconfigure(self.find_withtag("score"), text=self.score.get())
-
-    def onChangeLevel(self, arg1, arg2, arg3):
-        self.itemconfigure(self.find_withtag("level"), text=self.level.get())
-
     def shoot(self, e):
         if self.shootCooldown == 0:
             shotPos = self.coords(self.spaceship)[0] + 25
-            self.shotList.append((self.create_rectangle(shotPos-2, HEIGHT-50, shotPos+2, HEIGHT-60, width=0, fill="blue", tag="SpaceShipShot"), Shot(shotPos, 4, 10, 0, -3)))
+            self.shotList.append((self.create_rectangle(shotPos - 2, HEIGHT - 50, shotPos + 2, HEIGHT - 60, width=0, fill="blue", tag="SpaceShipShot"), Shot(shotPos, 4, 10, 0, -20)))
             self.shootCooldown = 50
 
     def checkHealth(self):
         if not self.health > 0:
-            self.create_text(WIDTH/2, HEIGHT/2 - 100, text="Game Over", font=tkFont.Font(size="70"), tag="GameOver")
-            #self.create_text(WIDTH/2, HEIGHT/2, text="Score: " + str(self.score.get()), font=tkFont.Font(size="70"), tag="finalScore")
+            self.create_text(WIDTH / 2, HEIGHT / 2 - 100, text="Game Over", font=tkFont.Font(size="70"), tag="GameOver")
+            self.create_text(WIDTH / 2, HEIGHT / 2, text="Score: " + str(self.score), font=tkFont.Font(size="70"), tag="finalScore")
             self.unbind_all("<a>")
             self.unbind_all("<d>")
             self.unbind_all("<space>")
         else:
             self.timer = self.after(DELAY, self.onTimer)
 
+    def spawnAliens(self):
+        if self.spawnDelay == 0:
+            self.spawnDelay = 30
+            if self.alienSpawnCount > 0:
+                self.alienList.append((self.create_rectangle(-20, 50, 0, 70, width=0, fill="red", tag="alien"), Alien()))
+                self.alienSpawnCount -= 1
+
+            if self.alienSpawnCount == 0 and len(self.find_withtag("alien")) == 0:
+                self.level += 1
+                if self.health < 5:
+                    self.health += 1
+                    self.menu.append(self.create_rectangle(WIDTH - (25 * self.health), 5, WIDTH - (25 * self.health) + 20, 25, fill="red", tag="health" + str(self.health)))
+                self.itemconfigure(self.find_withtag("level"), text=self.level)
+                self.alienSpawnCount = (self.level * 8)
+        else:
+            self.spawnDelay -= 1
+
     def onTimer(self):
         self.checkHealth()
-        self.alienSpawn += 1
-        if (self.alienSpawn > 30):
-            self.alienList.append((self.create_rectangle(-20, 50, 0, 70, width=0, fill="red", tag="alien"), Alien()))
-            self.alienSpawn = 0
+        self.spawnAliens()
         self.checkCollision()
         self.doMove()
         self.doShoot()
+
 
 class Game(Frame):
     def __init__(self):
         Frame.__init__(self)
         self.board = Board()
         self.pack()
+
 
 class Shot():
     def __init__(self, pos, sizex, sizey, movex, movey):
@@ -184,10 +204,11 @@ class Shot():
         self.movex = movex
         self.movey = movey
 
+
 def main():
     root = Tk()
     root.title("Space Invaders")
-    game = Game()
+    Game()
     root.mainloop()
 
 
