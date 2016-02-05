a367d79e5f935578a16e8307d06a349d3813ede6
raphael-s <r.schwob@4teamwork.ch>
2016-02-05 13:34:51 +0100

    Added more graphics & moved Shot class to own file

---
+++ py_files/shot.py
@@ -0,0 +1,8 @@
+class Shot():
+    def __init__(self, pos, sizex, sizey, movex, movey, id):
+        self.x = pos
+        self.sizex = sizex
+        self.sizey = sizey
+        self.movex = movex
+        self.movey = movey
+        self.id = id
